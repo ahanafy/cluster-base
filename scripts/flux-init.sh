c44a7ca78a0f558f3f731a066955a86b90e25524
Alan Hanafy <alan.hanafy@gmail.com>
2020-02-20 15:12:13 -0500

    update flux init script

--- scripts/flux-init.sh
+++ scripts/flux-init.sh
@@ -19,9 +19,6 @@ REPO_URL='https://github.com/ahanafy/cluster-base.git'
 # REPO_URL=${1:-git@github.com:stefanprodan/gitops-istio}
 REPO_BRANCH='cleanup'
 REPO_PUBLIC=true
-TEMP=${REPO_ROOT}/temp
-
-rm -rf ${TEMP} && mkdir ${TEMP}
 
 echo ">>> Creating Flux Namespace"
 kubectl apply -f ${REPO_ROOT}/base/fluxcd/namespace.yaml
@@ -37,44 +34,11 @@ helm upgrade -i fluxcd fluxcd/flux --wait --cleanup-on-fail \
 --set registry.pollInterval=1m \
 --namespace fluxcd
 
-cat <<EOF >> ${TEMP}/repositories.yaml
-configureRepositories:
-  enable: true
-  volumeName: repositories-yaml
-  secretName: flux-helm-repositories
-  cacheVolumeName: repositories-cache
-  repositories:
-    - caFile: ""
-      cache: stable-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: stable
-      password: ""
-      url: https://kubernetes-charts.storage.googleapis.com
-      username: ""
-    - caFile: ""
-      cache: istio.io-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: istio.io
-      password: ""
-      url: https://storage.googleapis.com/istio-release/releases/1.4.3/charts
-      username: ""
-    - caFile: ""
-      cache: flagger-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: flagger
-      password: ""
-      url: https://flagger.app
-      username: ""
-EOF
-
 echo ">>> Installing Helm Operator"
 helm upgrade -i helm-operator fluxcd/helm-operator --wait --cleanup-on-fail \
 --set git.ssh.secretName=fluxcd-git-deploy \
 --set helm.versions=v3 \
--f ${TEMP}/repositories.yaml \
+-f ${REPO_ROOT}/base/fluxcd/repositories-configmap.yaml \
 --namespace fluxcd
 
 echo '>>> GitHub deploy key'
